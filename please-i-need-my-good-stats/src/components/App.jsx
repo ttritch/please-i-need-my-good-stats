4487b90da2894c9982a1689fdfa0e76d63204589
Tristan Tritch <ttmeep101@hotmail.com>
2025-04-08 20:58:14 -0500

    ADD: added item json database, and completed the info for plate armor

--- please-i-need-my-good-stats/src/components/App.jsx
+++ please-i-need-my-good-stats/src/components/App.jsx
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from "react";
 
 function App () {
+  const [test, setTest] = useState("")
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:5000/season-2')
+    .then(resp => resp.json())
+    .then(data => {
+      setLoading(true)
+      console.log(data.plate.chest[0])
+      setTest(data.plate.chest[1].image)
+      console.log(test)
+    })
+  }, [isLoading])
+
   return <div>
-      <p>test</p>
+      <img src={test} />
   </div>
 }
 
