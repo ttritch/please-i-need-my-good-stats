c6412394287911f17d59bd95aa54b75f0b8ab77f
Tristan Tritch <ttmeep101@hotmail.com>
2025-04-23 18:43:45 -0500

    ADD: setup basic filters for armor types and misc items

--- please-i-need-my-good-stats/src/components/Filter.jsx
+++ please-i-need-my-good-stats/src/components/Filter.jsx
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({all}) {
-    console.log(all)
+function Filter({plate, mail, leather, cloth, misc, armorFilter, setArmorFilter, setShowMisc}) {
+    const [filterPlate, setFilterPlate] = useState(false)
+    const [filterMail, setFilterMail] = useState(false)
+    const [filterLeather, setFilterLeather] = useState(false)
+    const [filterCloth, setFilterCloth] = useState(false)
+    const [filterMisc, setFilterMisc] = useState(false)
+
     return <div>
-        <button>Plate</button>
-        <button>Mail</button>
-        <button>Leather</button>
-        <button>Cloth</button>
-        <button>Misc</button>
+        <button onClick={() => {
+            if(filterPlate == true) {
+                setArmorFilter([plate, mail, leather, cloth])
+                setFilterPlate(false)
+                setShowMisc(true)
+            }
+            else {
+                setArmorFilter([plate])
+                setShowMisc(false)
+                setFilterPlate(true)
+            }
+        }}>Plate</button>
+        <button onClick={() => {
+            if(filterMail == true) {
+                setArmorFilter([plate, mail, leather, cloth])
+                setFilterMail(false)
+                setShowMisc(true)
+            }
+            else {
+                setArmorFilter([mail])
+                setShowMisc(false)
+                setFilterMail(true)
+            }
+        }}>Mail</button>
+        <button onClick={() => {
+            if(filterLeather == true) {
+                setArmorFilter([plate, mail, leather, cloth])
+                setFilterLeather(false)
+                setShowMisc(true)
+            }
+            else {
+                setArmorFilter([leather])
+                setShowMisc(false)
+                setFilterLeather(true)
+            }
+        }}>Leather</button>
+        <button onClick={() => {
+            if(filterCloth == true) {
+                setArmorFilter([plate, mail, leather, cloth])
+                setFilterCloth(false)
+                setShowMisc(true)
+            }
+            else {
+                setArmorFilter([cloth])
+                setShowMisc(false)
+                setFilterCloth(true)
+            }
+        }}>Cloth</button>
+        <button onClick={() => {
+            if(filterMisc == true) {
+                setArmorFilter([plate, mail, leather, cloth])
+                setFilterMisc(false)
+                setShowMisc(true)
+            }
+            else {
+                setArmorFilter([])
+                setShowMisc(true)
+                setFilterMisc(true)
+            }
+        }}>Misc</button>
         <p></p>
         <button>Crit</button>
         <button>Mastery</button>
--- please-i-need-my-good-stats/src/components/Main.jsx
+++ please-i-need-my-good-stats/src/components/Main.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemBar from "./ItemBar";
 import Filter from "./Filter";
 
 function Main({plate, mail, leather, cloth, misc, all}) {
-    const itemTypes = ["head", "shoulder", "chest", "wrist", "hands", "waist", "legs", "feet"]
-    const armorTypes = [plate, mail, leather, cloth]
+    const [armorTypes, setArmorTypes] = useState([plate, mail, leather, cloth])
+    const [showMisc, setShowMisc] = useState(true)
+    const [itemTypes, setItemTypes] = useState(["head", "shoulder", "chest", "wrist", "hands", "waist", "legs", "feet"])
+    const [miscTypes, setMiscTypes] = useState(["neck", "back", "finger"])
     return <div>
         <h2> please i need my good stats</h2>
         <div>
-            <Filter all={all}/>
+            <Filter 
+                plate={plate}
+                mail={mail}
+                leather={leather}
+                cloth={cloth}
+                misc={misc}
+                armorFilter={armorTypes}
+                setArmorFilter={setArmorTypes}
+                setShowMisc={setShowMisc}
+            />
             {armorTypes.map((armor) => {
                 return itemTypes.map((type) => {
                     return armor[type].map((item) => {
@@ -25,6 +36,21 @@ function Main({plate, mail, leather, cloth, misc, all}) {
                         />
                     })
                 })})}
+            { showMisc ? (miscTypes.map((type) => {
+                return misc[type].map((item) => {
+                    return <ItemBar
+                        key={item.id}
+                        name={item.name}
+                        firstStat={item.firstStat}
+                        secondStat={item.secondStat}
+                        firstVal={item.firstVal}
+                        secondVal={item.secondVal}
+                        source={item.source}
+                        instance={item.instance}
+                        image={item.image}
+                    />
+                })
+            })) : (<></>)}
         </div>
     </div>
 }
