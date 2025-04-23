9dc1302802548d85c89e2c75c780941136b57b7c
Tristan Tritch <ttmeep101@hotmail.com>
2025-04-23 15:03:40 -0500

    ADD: fixed rendering error, now displays all items

--- please-i-need-my-good-stats/src/components/Main.jsx
+++ please-i-need-my-good-stats/src/components/Main.jsx
@@ -4,26 +4,27 @@ import Filter from "./Filter";
 
 function Main({plate, mail, leather, cloth, misc, all}) {
     const itemTypes = ["head", "shoulder", "chest", "wrist", "hands", "waist", "legs", "feet"]
-    const armorTypes = ["plate", "mail", "leather", "cloth", "misc"]
+    const armorTypes = [plate, mail, leather, cloth]
     return <div>
         <h2> please i need my good stats</h2>
         <div>
             <Filter all={all}/>
-            {itemTypes.map((type) => {
-                return plate[type].map((item) => {
-                    return <ItemBar
-                        key={item.id}
-                        name={item.name}
-                        firstStat={item.firstStat}
-                        secondStat={item.secondStat}
-                        firstVal={item.firstVal}
-                        secondVal={item.secondVal}
-                        source={item.source}
-                        instance={item.instance}
-                        image={item.image}
-                    />
-                })
-            })}
+            {armorTypes.map((armor) => {
+                return itemTypes.map((type) => {
+                    return armor[type].map((item) => {
+                        return <ItemBar
+                            key={item.id}
+                            name={item.name}
+                            firstStat={item.firstStat}
+                            secondStat={item.secondStat}
+                            firstVal={item.firstVal}
+                            secondVal={item.secondVal}
+                            source={item.source}
+                            instance={item.instance}
+                            image={item.image}
+                        />
+                    })
+                })})}
         </div>
     </div>
 }
